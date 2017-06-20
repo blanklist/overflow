47252c3d8cce0718d55c7084727d859b4261b426
Mike Tarkington & Joe Hiner <pair+MikeTarkington.blanklist@devbootcamp.com>
2017-06-19 18:23:49 -0700

    comments on questions, comments on answer, and routes and links

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  # def index
+  #   @answers = Answer.all
+  #   # render :index
+  # end
+  #
+  # def show
+  #   @answer = Answer.find(params[:id])
+  # end
+
+  # def new
+  # end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,6 +6,8 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
+    # @comments = @question.comments
+    @answers = @question.answers
   end
 
 end
