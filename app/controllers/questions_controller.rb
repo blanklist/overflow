9e466403bb8b7fb844b13d704f5f4a509779795d
Mike Tarkington & Joe Hiner <pair+MikeTarkington.blanklist@devbootcamp.com>
2017-06-19 17:31:21 -0700

    show page for questions, routes for answers and comments, links for question show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,4 +4,8 @@ class QuestionsController < ApplicationController
     # render :index
   end
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :question do
+  resources :questions do
     resources :answers
+    resources :comment
+  end
+
+  resources :answers do
+    resources :comment
   end
 
   root "questions#index"
